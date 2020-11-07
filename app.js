c24b2879fba9cee5290d422ff59a8108a7705ae3
Lucas Pereira e Silva <lucas.p.silva90@hotmail.com>
2020-11-07 18:04:14 -0300

    preparando para o deploy no gcp

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://usuario_admin:nbwx6nqY0Pr1E0sp@cluster0.huse4.mongodb.net/desafio_sky?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });  
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var usersRouter = require('./routes/users');
 
